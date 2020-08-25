7c8ab3c86abbbdc4c1ddd2cd347c2f02228dd185
Cesar Villamil <cesar.villamil@mail.escuelaing.edu.co>
2020-08-24 22:43:13 -0500

    OperacionesMatrices

--- ComplexLibrary.py
+++ ComplexLibrary.py
@@ -55,3 +55,145 @@ def gradRadi(num):
     return (num*180)/math.pi
 
 
+def adicionVectores(a,b):
+    ans=[0*len(a) for n in range(len(a))]
+    if(len(a)!=len(b)):
+        return "Las dimensiones de los vectores no son iguales"
+    else:
+        for i in range(len(a)):
+            ans[i] = complexSum(a[i],b[i])
+        return ans
+
+def restaVectores(a,b):
+    ans=[0*len(a) for n in range(len(a))]
+    if(len(a)!=len(b)):
+        return "Las dimensiones de los vectores no son iguales"
+    else:
+        for i in range(len(a)):
+            ans[i] = complexRest(a[i],b[i])
+        return ans
+def inversaVector(a):
+    ans=[0*len(a) for n in range(len(a))]
+    for i in range(len(a)):
+        ans[i] = (-1*(a[i][0]),-1*(a[i][1]))
+    return ans
+
+def escalarXComplejo(e,v):
+    ans=[[0]*len(v) for n in range(len(v))]
+    for i in range(len(v)):
+        real=e*v[i][0]
+        complejo=e*v[i][1]
+        ans[i]=(real,complejo)
+    return ans
+def sumaMatrices(a,b):
+    ans=[[0]*len(a) for n in range(len(a))]
+    if(len(a)!=len(b)):
+        return "Las dimensiones de los vectores no son iguales"
+    else:
+        for i in range(len(a)):
+            for j in range(len(a[i])):
+                ans[i][j]=complexSum(a[i][j],b[i][j])
+    return ans
+
+def multiply(v, G):
+    ans = []
+    for i in range(len(G[0])): 
+        total = 0
+        for j in range(len(v)): 
+            total += v[j] * G[j][i]
+        ans.append(total)
+    return ans
+
+def inversaMatriz(a):
+    ans=[[0]*len(a) for n in range(len(a))]
+    for i in range(len(a)):
+        ans[i] = inversaVector(a[i])
+    return ans
+
+def escalarXMatriz(e,a):
+    ans=[[0]*len(a) for n in range(len(a))]  
+    for i in range(len(a)):
+        ans[i]=escalarXComplejo(e,a[i])        
+    return ans
+
+def transpuestaMatriz(a):
+    ans=[[0]*len(a) for n in range(len(a[0]))]
+    
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            ans[j][i]=a[i][j]
+    return ans
+
+def conjugadoMatriz(a):
+    ans=[[0]*len(a) for n in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            ans[i][j]=complexConj(a[i][j])
+    return ans
+
+def dagaMatriz(a):
+    ans=transpuestaMatriz(a)
+    ans=conjugadoMatriz(a)
+    return ans
+
+def matrixProduct(a,b):
+    ans=[[0]*len(b[0]) for n in range(len(a))]
+
+    
+    if(len(a[0])!=len(b)):
+        return "No se pueden multiplicar estas matrices"
+    else:
+        for i in range(len(a)):
+            for j in range(len(b[0])):
+                for k in range(len(b)):
+                    ans[i][j] += a[i][k] * b[k][j]
+
+    return ans
+
+def conjugadoVector(a):
+    ans = [0]*len(a)
+    for i in range(len(a)):
+        ans[i] = complexConj(a[i])
+    return ans 
+
+def multiVectores(a,b):
+    ans = (0,0) 
+    for i in range(len(a)):
+        ans = complexSum(ans,complexProduct(a[i],b[i]))
+    return ans
+    
+def innerProduct(a,b):
+    v1 = conjugadoVector(a)
+    return multiVectores(v1,b)
+
+def normaVector(a):
+    norma = innerProduct(a,a)
+    return math.sqrt(norma[0])
+
+def distanciaVectores(a,b):
+    resta = restaVectores(a,b)
+    return normaVector(resta)
+def esUnitaria(a):
+    adjunta = dagaMatriz(a)
+    ans = matrixProduct(a,adjunta)
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            num = ans[i][j]
+            if(i==j and (num[0]!=1 or num[1]!=0)):
+                return False
+            elif(i!=j and (num[0]!=0 or num[1]!=0)):
+                 return False
+    return True
+def esHermitiana(a):
+    adjunta = dagaMatriz(a)
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            if(a[i][j]!=adjunta[i][j]):
+                return False
+    return True
+def productoTensorVectores(a,b):
+    ans = []
+    for i in range(len(a)):
+        for j in range(len(b)):
+            ans.append(complexProduct(a[i],b[j]))
+    return ans 
--- Pruebas.py
+++ Pruebas.py
@@ -1,7 +1,6 @@
 import unittest
-import ComplexLibrary
 import math 
-
+import ComplexLibrary
 class pruebaComplejos(unittest.TestCase):
 
     def test_sum(self):
@@ -52,5 +51,108 @@ class pruebaComplejos(unittest.TestCase):
         self.assertEqual(ComplexLibrary.complexArg((3,21)),1.4288992721907328)
         self.assertEqual(ComplexLibrary.complexArg((6,-76)),-1.492012365805753)
         self.assertEqual(ComplexLibrary.complexArg((5,16)),1.2679114584199251)
+
+    
+    def test_sumaVectores(self):
+        ans1 = ComplexLibrary.adicionVectores([(1,2),(1,2),(3,5)],[(-2,2),(2,2),(4,10)])
+        ans2 = ComplexLibrary.adicionVectores([(1,0),(1,-2),(3,2)],[(2,2),(3,2),(4,5)])
+        self.assertEqual(ans1,[(-1,4),(3,4),(7,15)])
+        self.assertEqual(ans2,[(3,2),(4,0),(7,7)])
+        
+    def test_inversaVector(self):
+        ans1 = ComplexLibrary.inversaVector([(1,-1),(2,3),(-1,3)])
+        ans2 = ComplexLibrary.inversaVector([(1,2),(-3,3),(-1,3)])
+        self.assertEqual(ans1,[(-1,1),(-2,-3),(1,-3)])
+        self.assertEqual(ans2,[(-1,-2),(3,-3),(1,-3)])
+        
+    def test_multiplicacionVectores(self):
+        ans1 = ComplexLibrary.matrixProduct([(1,2),(3,4),(1,5)],(0,0))
+        ans2 = ComplexLibrary.matrixProduct((1,1),[(1,2),(3,4),(1,5)])
+        self.assertEqual(ans1,[(0,0),(0,0),(0,0)])
+        self.assertEqual(ans2,[(-1,3),(-1,7),(-4,6)])
+
+    def test_sumaMatrices(self):
+        ans1 = ComplexLibrary.sumaMatrices([[(1,1),(2,-3)],[(4,2),(2,4)]],[[(1,0),(1,0)],[(1,0),(1,0)]])
+        ans2 = ComplexLibrary.sumaMatrices([[(1,3),(2,3)],[(1,3),(2,3)]],[[(1,0),(3,6)],[(1,0),(3,6)]])
+        self.assertEqual(ans1,[[(2,1),(3,-3)],[(5,2),(3,4)]])
+        self.assertEqual(ans2,[[(2,3),(5,9)],[(2,3),(5,9)]])
+
+    def test_inversaMatriz(self):
+        ans1 = ComplexLibrary.inversaMatriz([[(1,1),(2,-3)],[(4,2),(2,4)]])
+        ans2 = ComplexLibrary.inversaMatriz([[(1,1),(0,3)],[(-1,2),(-2,4)]])
+        self.assertEqual(ans1,[[(-1,-1),(-2,3)],[(-4,-2),(-2,-4)]])
+        self.assertEqual(ans2,[[(-1,-1),(0,-3)],[(1,-2),(2,-4)]])
+
+    def multiplicacionEscalarMatrices(self):
+        ans1 = ComplexLibrary.escalarXMatriz([[(1,1),(2,-3)],[(4,2),(2,4)]],(1,1))
+        ans2 = ComplexLibrary.escalarXMatriz([[(1,1),(2,-3)],[(4,2),(2,4)]],(2,3))
+        self.assertEqual(ans1,[[(0,2),(5,-1)],[(2,6),(-2,6)]])
+        self.assertEqual(ans2,[[(-1,5),(13,0)],[(2,16),(-8,14)]])
+
+    def test_traspuesta(self):
+        ans1 = ComplexLibrary.transpuestaMatriz([[(1,1),(2,-3)],[(4,2),(2,4)]])
+        ans2 = ComplexLibrary.transpuestaMatriz([[(1,1),(2,-3),(5,4)],[(4,2),(2,4),(4,5)]])
+        self.assertEqual(ans1,[[(1,1),(4,2)],[(2,-3),(2,4)]])
+        self.assertEqual(ans2,[[(1,1),(4,2)],[(2,-3),(2,4)],[(5,4),(4,5)]])
+
+    def test_conjugadoMatriz(self):
+        ans1 = ComplexLibrary.conjugadoMatriz([[(1,2),(2,1)],[(1,-2),(2,-3)],[(3,4),(3,-1)]])
+        ans2 = ComplexLibrary.conjugadoMatriz([[(1,1),(2,2),(1,1)],[(2,2),(3,-3),(2,3)]])
+        self.assertEqual(ans1,[[(1,-2),(2,-1)],[(1,2),(2,3)],[(3,-4),(3,1)]])
+        self.assertEqual(ans2,[[(1,-1),(2,-2),(1,-1)],[(2,-2),(3,3),(2,-3)]])
+
+    def test_matrizAdjunta(self):
+        ans1 = ComplexLibrary.dagaMatriz([[(1,1),(2,-3)],[(4,2),(2,4)]])
+        ans2 = ComplexLibrary.dagaMatriz([[(1,1),(2,3)],[(4,2),(1,4)],[(1,0),(3,-4)]])
+        self.assertEqual(ans1,[[(1,-1),(4,-2)],[(2,3),(2,-4)]])
+        self.assertEqual(ans2,[[(1,-1),(4,-2),(1,0)],[(2,-3),(1,-4),(3,4)]])
+
+    def test_productoVectorMatriz(self):
+        ans1 = ComplexLibrary.multiply([(1,1),(2,3)],[[(1,2),(2,1)],[(3,3),(1,-2)]])
+        ans2 = ComplexLibrary.multiply([(0,0),(0,0)],[[(1,2),(2,1)],[(3,3),(1,-2)]])
+        self.assertEqual(ans1,[(0,11),(8,5)])
+        self.assertEqual(ans2,[(0,0),(0,0)])
+
+    def test_productoInternoVectores(self):
+        ans1 = ComplexLibrary.innerProduct([(1,0),(0,1),(1,-3)],[(2,1),(0,1),(2,0)])
+        ans2 = ComplexLibrary.innerProduct([(1,-2),(-2,1),(4,3)],[(2,1),(1,-3),(3,0)])
+        self.assertEqual(ans1,(5,7))
+        self.assertEqual(ans2,(7,1))
+
+    def test_normaMatriz(self):
+        self.assertEqual(ComplexLibrary.normaVector([(3,0),(-6,0),(-2,0)]),7.0)
+        self.assertEqual(ComplexLibrary.normaVector([(2,0),(4,0)]),4.47213595499958)
+        self.assertEqual(ComplexLibrary.normaVector([(4,0),(3,0)]),5.0)
+
+    def test_distanciaVectores(self):
+        ans1 = ComplexLibrary.distanciaVectores([(0,0),(0,0)],[(1,1),(2,4)])
+        ans2 = ComplexLibrary.distanciaVectores([(1,2),(2,5)],[(2,6),(5,3)])
+        self.assertEqual(ans1,4.69041575982343)
+        self.assertEqual(ans2,5.477225575051661)
+        
+
+    
+    def test_esUnitaria(self):
+        self.assertFalse(ComplexLibrary.esUnitaria([[(1,1),(0,0)],[(0,0),(1,-1)]]))
+        self.assertFalse(ComplexLibrary.esUnitaria([[(-1,0),(0,-1)],[(0,1),(1,0)]]))
+        self.assertTrue(ComplexLibrary.esUnitaria([[(0,0),(1,0)],[(1,0),(0,0)]]))
+        
+    def test_esHermitiana(self):
+        self.assertFalse(ComplexLibrary.esHermitiana([[(1,1),(0,0)],[(0,0),(1,-1)]]))
+        self.assertTrue(ComplexLibrary.esHermitiana([[(-1,0),(0,-1)],[(0,1),(1,0)]]))
+        self.assertTrue(ComplexLibrary.esUnitaria([[(0,0),(1,0)],[(1,0),(0,0)]]))
+
+    def test_productoTensorVectores(self):
+        ans1 = ComplexLibrary.productoTensorVectores([(-1,0),(2,0),(5,0)],[(4,0),(-3,0)])
+        ans2 = ComplexLibrary.productoTensorVectores([(-1,1),(2,-1),(5,1)],[(4,1),(3,0)])
+        self.assertEqual(ans1,[(-4,0),(3,0),(8,0),(-6,0),(20,0),(-15,0)])
+        self.assertEqual(ans2,[(-5,3),(-3,3),(9,-2),(6,-3),(19,9),(15,3)])
+        
+
+    def test_productoTensorMatrices(self):
+        ans1 = ComplexLibrary.productoTensorMatrices([[(1,0),(2,0)],[(0,0),(1,0)]],[[(3,0),(2,0)],[(-1,0),(0,0)]])
+        ans2 = ComplexLibrary.productoTensorMatrices([(0,0),(0,0)],[[(1,0),(2,0)],[(3,0),(2,0)],[(1,1),(2,2)]])
+        self.assertEqual(ans1,[[(3,0),(2,0),(6,0),(4,0)],[(-1,0),(0,0),(-2,0),(0,0)],[(0,0),(0,0),(3,0),(2,0)],[(0,0),(0,0),(-1,0),(0,0)]])
+        self.assertEqual(ans2,[[(0,0),(0,0),(0,0),(0,0)],[(0,0),(0,0),(0,0),(0,0)],[(0,0),(0,0),(0,0),(0,0)]])
 if __name__ == "__main__":
     unittest.main()
